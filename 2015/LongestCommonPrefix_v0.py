7d4831dfa078fe028581c7e4290a208c8d35b7f6
everbird <stephen.zhuang@gmail.com>
2015-05-05 08:54:29 -0700

    Longest Common Prefix sovled

--- 2015/LongestCommonPrefix_v0.py
+++ 2015/LongestCommonPrefix_v0.py
@@ -9,21 +9,20 @@ class Solution:
         if not strs:
             return ''
 
+        min_l = min(map(len, strs))
         i = 0
-        prefix = ''
-        first_str = strs[0]
-        lenf = len(first_str)
-        same = True
-        while same:
-            for s in strs:
-                same &= ((i <= (len(s) - 1)) and (i <= (lenf - 1)) and s[i] == first_str[i])
-
-            if same:
-                prefix += first_str[i]
-
+        while (i < min_l) and all_same(map(lambda x: x[i], strs)):
             i += 1
 
-        return prefix
+        return strs[0][:i] if i > 0 else ''
+
+
+def all_same(array):
+    for i in range(len(array) - 1):
+        if array[i] != array[i + 1]:
+            return False
+
+    return True
 
 
 if __name__ == '__main__':
@@ -33,6 +32,7 @@ if __name__ == '__main__':
         'abc',
     ]
     #a = ["","cbb",""]
+    #a = ['a', 'b']
     s = Solution()
     r = s.longestCommonPrefix(a)
     print r
