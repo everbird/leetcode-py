9d4ff185d0d9a06c996051489ee9451dd6c0a629
everbird <stephen.zhuang@gmail.com>
2015-05-07 18:32:11 -0700

    Combination Sum II sovled, but it is slow

---
+++ 2015/CombinationSumII_v0.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+class Solution:
+    # @param {integer[]} candidates
+    # @param {integer} target
+    # @return {integer[][]}
+    def combinationSum2(self, candidates, target):
+        candidates.sort()
+        return comination(candidates, target)
+
+
+def comination(candidates, k):
+    if k < 0:
+        return []
+
+    if k == 0:
+        return [[]]
+
+    r = []
+    for i, n in enumerate(candidates):
+        items = comination(candidates[i + 1:], k - n)
+        for item in items:
+            if not item:
+                item.append(n)
+            else:
+                item = [n] + item
+
+            if item not in r:
+                r.append(item)
+
+    return r
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print s.combinationSum2([10,1,2,7,6,1,5], 8)
