e83bbe9ff63d5bfa2a853f6bac220f94a74a15de
everbird <stephen.zhuang@gmail.com>
2015-05-22 16:10:09 -0700

    Populating Next Right Pointers in Each Node II solved using level order traversal

---
+++ 2015/PopulatingNextRightPointersInEachNodeII_v0.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+# Definition for binary tree with next pointer.
+class TreeLinkNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+        self.next = None
+
+
+class Solution:
+    # @param root, a tree link node
+    # @return nothing
+    def connect(self, root):
+        if not root:
+            return
+
+        queue = [(0, root)]
+        pre = None
+        pre_level = 0
+        while queue:
+            level, n = queue.pop()
+
+            if pre:
+                if pre_level == level:
+                    pre.next = n
+
+            if n.left:
+                queue = [(level+1, n.left)] + queue
+
+            if n.right:
+                queue = [(level+1, n.right)] + queue
+
+            pre = n
+            pre_level = level
+
+    def levelorder(self, root):
+        queue = [root]
+        while queue:
+            n = queue.pop()
+            print n, n.val, n.next, '<<<'
+            if n.left:
+                queue = [n.left] + queue
+
+            if n.right:
+                queue = [n.right] + queue
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    n1 = TreeLinkNode(1)
+    n2 = TreeLinkNode(2)
+    n3 = TreeLinkNode(3)
+    n4 = TreeLinkNode(4)
+    n5 = TreeLinkNode(5)
+    n6 = TreeLinkNode(6)
+    n7 = TreeLinkNode(7)
+
+    root = n1
+    n1.left = n2
+    n1.right = n3
+    n2.left = n4
+    n2.right = n5
+    n3.left = n6
+    n3.right = n7
+
+    s.connect(root)
+    s.levelorder(root)
