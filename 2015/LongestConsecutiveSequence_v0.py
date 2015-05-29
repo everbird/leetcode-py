23a877ceabaaaa2449e5d345f63fdd9f2f011ac5
everbird <stephen.zhuang@gmail.com>
2015-05-28 21:35:01 -0700

    Longest Consecutive Sequence TLE, O(N^2) solution

---
+++ 2015/LongestConsecutiveSequence_v0.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer}
+    def longestConsecutive(self, nums):
+        d = {}
+        for i in range(len(nums)):
+            n = nums[i]
+            d[n] = True
+
+        max_c = 0
+        for i in range(len(nums)):
+            count = 0
+            n = nums[i]
+            while d.get(n):
+                n -= 1
+                count += 1
+
+            if max_c < count:
+                max_c = count
+
+        return max_c
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print s.longestConsecutive([100, 4, 200, 1, 3, 2])
