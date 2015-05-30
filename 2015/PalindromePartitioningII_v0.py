75a3c75fcfb6546da1e4149951e3ef4d9c2588c1
everbird <stephen.zhuang@gmail.com>
2015-05-29 19:58:37 -0700

    Palindrome Partitioning II solved.

---
+++ 2015/PalindromePartitioningII_v0.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+class Solution:
+    # @param {string} s
+    # @return {integer}
+    def minCut(self, s):
+        lens = len(s)
+        r = [0] * (lens + 1)
+        for i in range(lens+1):
+            r[i] = i - 1
+
+        for i in range(lens):
+            for j in range(i+1):
+                if i+j < lens and s[i-j] == s[i+j]:
+                    r[i+j+1] = min(r[i+j+1], r[i-j] + 1)
+
+            for j in range(1, i+2):
+                if i+j < lens and s[i-j+1] == s[i+j]:
+                    r[i+j+1] = min(r[i+j+1], r[i-j+1] + 1)
+
+        return r[lens]
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print s.minCut('a')
+    print s.minCut('ab')
+    print s.minCut('aab')
+    print s.minCut('')
+    print s.minCut('aabcbd')
+    print s.minCut('fifgbeajcacehiicccfecbfhhgfiiecdcjjffbghdidbhbdbfbfjccgbbdcjheccfbhafehieabbdfeigbiaggchaeghaijfbjhi')
