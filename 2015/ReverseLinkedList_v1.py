c8932ce589f2e5693661fe8b3ac1efd665d60116
everbird <stephen.zhuang@gmail.com>
2015-06-13 16:17:42 -0700

    Reverse Linked List solved using iteration

---
+++ 2015/ReverseLinkedList_v1.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __repr__(self):
+        return '<{}>'.format(self.val)
+
+
+def printl(n):
+    if n:
+        print n
+        printl(n.next)
+
+
+class Solution:
+    # @param {ListNode} head
+    # @return {ListNode}
+    def reverseList(self, head):
+        pre = None
+        p = head
+        while p:
+            _next = p.next
+            p.next = pre
+            pre = p
+            p = _next
+
+        return pre
+
+
+if __name__ == '__main__':
+    s = Solution()
+    n1 = ListNode(1)
+    n2 = ListNode(2)
+    n3 = ListNode(3)
+    n4 = ListNode(4)
+    n5 = ListNode(5)
+    h = n1
+    n1.next = n2
+    n2.next = n3
+    n3.next = n4
+    n4.next = n5
+
+    printl(h)
+    nh = s.reverseList(h)
+    print '-' * 6
+    printl(nh)
