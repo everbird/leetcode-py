ebe53fa021fe574cb5da84a2d4a50fcd2b1d636f
everbird <stephen.zhuang@gmail.com>
2015-06-19 16:32:12 -0700

    Transpose File solved accroding to discussion using awk

---
+++ 2015/TransposeFile.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+
+cat file2.txt | awk '
+{
+    for (i = 1; i <= NF; i++) {
+        if(NR == 1) {
+            s[i] = $i;
+        } else {
+            s[i] = s[i] " " $i;
+        }
+    }
+}
+END {
+    for (i = 1; s[i] != ""; i++) {
+        print s[i];
+    }
+}'
